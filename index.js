6ebb7a7a8caf0529b20f38418181c6fd19ac4683
Gunajn Bansal <39277147+gunjanbansal9512@users.noreply.github.com>
2020-04-23 23:33:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 var data;
-$(document).ready( function () {
+$(document).ready(function() {
     data = $.getJSON("http://starlord.hackerearth.com/gamesext", function(data) {
-     //  console.log(data);
-       for(var i=0;i<data.length;i++){
-        // console.log(data[i]);
-        var row = '  <div class="col-sm-4 py-2" id="cards" ><div class="card card-body h-100" style="width: 20rem; display:inline-block;  margin:auto;  ">';
-        row+=' <img class="card-img-top" src="image.jpg" alt="Card image cap">';
-        row+='<div class="card-body">';
-        row+=  '<h5 class="card-title" id="title">Title : '+data[i]["title"]+'</h5>';
-       row+= '<p class="card-text">URL : '+data[i]["url"]+'</p><p class="card-text">Platform : '+data[i]["platform"]+'</p><p class="card-text">Score : '+data[i]["score"]+'</p><p class="card-text">Genre : '+data[i]["genre"]+'</p><p class="card-text">Editiors Choice : '+data[i][ "editors_choice"]+'</p><p class="card-text">Release Year : '+data[i][ "release_year"]+'</p></div></div></div>';
-  $('#gameData').append(row);
-    }
-    
+        //  console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            // console.log(data[i]);
+            var row = '  <div class="col-sm-4 py-2" id="cards" ><div class="card card-body h-100" style="width: 20rem; display:inline-block;  margin:auto;  ">';
+            row += ' <img class="card-img-top" src="image.jpg" alt="Card image cap">';
+            row += '<div class="card-body">';
+            row += '<h5 class="card-title" id="title">Title : ' + data[i]["title"] + '</h5>';
+            row += '<p class="card-text">URL : ' + data[i]["url"] + '</p><p class="card-text">Platform : ' + data[i]["platform"] + '</p><p class="card-text">Score : ' + data[i]["score"] + '</p><p class="card-text">Genre : ' + data[i]["genre"] + '</p><p class="card-text">Editiors Choice : ' + data[i]["editors_choice"] + '</p><p class="card-text">Release Year : ' + data[i]["release_year"] + '</p></div></div></div>';
+            $('#gameData').append(row);
+        }
+
     });
 
-  
-    
+
+
 });
 
+
 $("#searchBtn").on("click", function() {
-  var searchText = $("#search").val().trim();
-  console.log(searchText);  
-  //console.log(data.responseJSON);
-  let restJsondata = data.responseJSON;
-  let filteredData =[];
-  for(var i=0;i<restJsondata.length;i++){
-   //console.log(restJsondata[i]['title'].toLowerCase());
-   var title =restJsondata[i]['title']; 
-   if(title !==null ){
-    //console.log(title);
-   if(title.toString().toLowerCase().search(searchText.toLowerCase())){
-     console.log(title);
-      filteredData.push[restJsondata[i]];
-    }
-   }
+    $('#gameData').empty(); //clean
+    var searchText = $("#search").val().trim();
    
-  }
-  console.log(filteredData);
-  // $("#gameData").children().children().find("div").show().filter(function() {
-  //   return $(this).text().toLowerCase().trim().indexOf(value) == -1;
-  // }).hide();
-  //console.log($("#gameData").children().children());
+    let restJsondata = data.responseJSON;
+    var filteredData = [];
+    for (var i = 0; i < restJsondata.length; i++) {
+        var title = restJsondata[i]['title'];
+        if (title) {
+            if (title.toString().toLowerCase().search(searchText.toLowerCase())==0) {
+                console.log(restJsondata[i]);
+                var row = '  <div class="col-sm-4 py-2" id="cards" ><div class="card card-body h-100" style="width: 20rem; display:inline-block;  margin:auto;  ">';
+                row += ' <img class="card-img-top" src="image.jpg" alt="Card image cap">';
+                row += '<div class="card-body">';
+                row += '<h5 class="card-title" id="title">Title : ' + restJsondata[i]["title"] + '</h5>';
+                row += '<p class="card-text">URL : ' + restJsondata[i]["url"] + '</p><p class="card-text">Platform : ' + restJsondata[i]["platform"] + '</p><p class="card-text">Score : ' + restJsondata[i]["score"] + '</p><p class="card-text">Genre : ' + restJsondata[i]["genre"] + '</p><p class="card-text">Editiors Choice : ' + restJsondata[i]["editors_choice"] + '</p><p class="card-text">Release Year : ' + restJsondata[i]["release_year"] + '</p></div></div></div>';
+                $('#gameData').append(row);
+            }
+        }
+
+    }
 });
