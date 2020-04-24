917621259b1c4f092c31221e1b85cdd10d40d108
gunjanbansal9512 <gunjanbansalcitech@gmail.com>
2020-04-24 21:21:57 +0530

    game arena

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+(function(){
+  var myDiv = document.getElementById("loading"),
+
+    show = function(){
+      myDiv.style.display = "block";
+      setTimeout(hide, 2000);
+      document.getElementById("site").style.display="none";
+    },
+
+    hide = function(){
+    
+      myDiv.style.display = "none";
+      document.getElementById("site").style.display="block";
+    };
+
+  show();
+})();
+
 var data;
 var titlesAutoComplete = [];
     //["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
@@ -16,14 +34,13 @@ $(document).ready(function() {
 });
 
 
-// Search
 function getCardRender(gameObj) {
     const row =
         `<div class="col-sm-3" id="cards" style="margin-top:8px ">
     <div class="card card-body h-100">
         <img class="card-img-top" src="image.jpg" alt="Card image cap">
         <div>
-            <h6 class="font-weight-bold text-center mt-1" id="title">Title : ${gameObj['title']} </h6>
+            <h6 class="font-weight-bold mt-1" id="title">Title : ${gameObj['title']} </h6>
             <div class="container">
                 <div class="row text-capitalize">
                     <span class="col-12"> <span class="font-weight-bold">Platform :</span> ${gameObj['platform']} </span>
@@ -39,7 +56,7 @@ function getCardRender(gameObj) {
 </div>`
     return row;
 }
-
+// Search
 $("#searchBtn").on("click", function() {
     $('#gameData').empty(); //clean
     var searchText = $("#search").val().trim();
@@ -111,7 +128,7 @@ function autocomplete(inp, arr) {
       /*for each item in the array...*/
       for (i = 0; i < arr.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        if (arr[i].toString().substr(0, val.length).toUpperCase() == val.toUpperCase()) {
           /*create a DIV element for each matching element:*/
           b = document.createElement("DIV");
           /*make the matching letters bold:*/
